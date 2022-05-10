760d7116c82e3774561e7f281803ed049fcecf88
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-09 23:56:16 -0300

    first exercises

---
+++ src/chapter_01/Exerc01_party.java
@@ -0,0 +1,9 @@
+package chapter_01;
+
+public class Exerc01_party {
+
+    public static void main (String[] args) {
+        System.out.println("I rule!");
+        System.out.println("The world");
+    }
+}
---
+++ src/chapter_01/Exerc02_dooBeeDooBeeDo.java
@@ -0,0 +1,16 @@
+package chapter_01;
+
+public class Exerc02_dooBeeDooBeeDo {
+
+    public static void main (String[] args) {
+        int x = 1;
+        while (x < 5) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 1) {
+            System.out.print("Do");
+        }
+    }
+}
